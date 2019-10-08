fde89fb0b5450ee8b9eea1404370d6fa87519605
shing_glb <2188085133@qq.com>
2019-10-08 22:10:10 +0800

    学习demo3

--- src/leecode/longStringWithoutRepeat.java
+++ src/leecode/longStringWithoutRepeat.java
@@ -14,12 +14,12 @@ public class longStringWithoutRepeat {
         int max  = 0;
         int n = s.length();
         while(right < n){
-            if(used[s.charAt(right)] == false){
+            if(used[s.charAt(right)] == false){  //得到某个字符的位置，应为会返回收个该值得索引，第二次有重复值，会返回以第一次好到的索引，可以判断是否中饭，，然后将指针加1
                 used[s.charAt(right)] = true;
                 right++;
             }else {
                 max = Math.max(max,right  - left);
-                while (left<right && s.charAt(right)!=s.charAt(left)){
+                while (left<right && s.charAt(right)!=s.charAt(left)){//将left指向相同值得前一位
                     used[s.charAt(left)] = false;
                     left++;
                 }
